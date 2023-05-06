36359941d462150c7710faf5796ed22a97d715d1
NgoMauTruongQB <truongngo2707@gmail.com>
2023-05-06 11:14:31 +0700

    add bootstrap lib

---
+++ ananas_shop/src/components/CompTopHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="top-header row">Test top header</div>
+</template>
+
+<script>
+export default {
+  name: "top-header",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
